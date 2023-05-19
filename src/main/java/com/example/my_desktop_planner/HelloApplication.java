bc81245ce62555c5354669ab6b147ebe988f876f
Abdelkebir Achraf <104693339+aachraf94@users.noreply.github.com>
2023-05-19 22:58:47 +0100

    ajouter SeConnecterController et InscriptionController

--- src/main/java/com/example/my_desktop_planner/HelloApplication.java
+++ src/main/java/com/example/my_desktop_planner/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.my_desktop_planner;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,10 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("SeConnecter.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("My Desktop Planner");
+        //Image icon = new Image("Group42.png");
         stage.setScene(scene);
+        //stage.getIcons().add(icon);
         stage.show();
     }
 
