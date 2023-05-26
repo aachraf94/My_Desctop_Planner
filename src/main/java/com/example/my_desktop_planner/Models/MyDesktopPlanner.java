ba143d48e0921ec586b041d7633f80d774bcc75c
Abdelkebir Achraf <104693339+aachraf94@users.noreply.github.com>
2023-05-26 05:50:21 +0100

    changer le code du class my MyDesktopPlanner

--- src/main/java/com/example/my_desktop_planner/Models/MyDesktopPlanner.java
+++ src/main/java/com/example/my_desktop_planner/Models/MyDesktopPlanner.java
@@ -9,8 +9,9 @@ import java.util.HashMap;
 import static com.example.my_desktop_planner.HelloApplication.*;
 
 public class MyDesktopPlanner implements Serializable {
-    private static MyDesktopPlanner instance;
     private HashMap<Utilisateur, String> utilisateurs;
+
+    /**************  constructor  ****************************************************/
 
     public MyDesktopPlanner() {
         this.utilisateurs = new HashMap<Utilisateur, String>();
@@ -19,57 +20,14 @@ public class MyDesktopPlanner implements Serializable {
         this.utilisateurs.put(new Utilisateur("achraf","achraf"),"achraf");
         this.utilisateurs.put(new Utilisateur("chamel","chamel"),"chamel");
     }
-
+    /*********************************************************************************/
 
     public HashMap<Utilisateur, String> getUtilisateurs() {
         return utilisateurs;
     }
 
+    /*********************************************************************************/
 
-
-
-
-
-    //on va faire Method registre
-
-    public void addUser(Utilisateur user, String mdp) {// verifié si l'utilisateur existe ou non
-        if (!utilisateurs.containsKey(user)) {
-            utilisateurs.put(user, mdp);// ajouter le couple utilisateur et mdp
-            System.out.println("User '" + user.getPseudo() + "' added successfully.");
-            saveUsersToFile();
-        } else {
-            System.out.println("User '" + user.getPseudo() + "' already exists.");
-        }
-    }
-
-    public void removeUser(Utilisateur user) {// verifié si l'utilisateur existe ou non
-        if (utilisateurs.containsKey(user)) {
-            utilisateurs.remove(user);// supprimer le couple utilisateur et mdp
-            System.out.println("User '" + user.getPseudo() + "' removed successfully.");
-            saveUsersToFile();
-        } else {
-            System.out.println("User '" + user.getPseudo() + "' does not exist.");
-        }
-    }
-
-    public void saveUsersToFile() {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Users.dat"))) {// on cree un nouveu flux de sortie pour le fichier Users.dat pour le modifier
-            out.writeObject(utilisateurs);// enregistrer le hashMap dans le fichier
-            System.out.println("Saved successfully");
-        } catch (IOException e) {
-            throw new RuntimeException("Error saving users to file.", e);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public void loadUsersFromFile() {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("Users.dat"))) {//users.dat c'est le fichier qui contient les utilisateurs , il sera ecrasé tout fois qu'on ajoute ou on supprime un utilisateur
-            utilisateurs = (HashMap<Utilisateur, String>) in.readObject();
-            System.out.println("Loaded successfully");//lire le fichier et le mettre dans le hashMap puisaue la class, est singleton , on peut le faire directement
-        } catch (IOException | ClassNotFoundException e) {
-            // If the file doesn't exist or there's an error reading it, ignore and start with an empty HashMap
-        }
-    }
 
 
 //    cheked and updated
